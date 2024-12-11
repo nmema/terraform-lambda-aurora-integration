aae8f902ee288f637e57a3e297528f07425aeca1
Nicolas Mema <nicolas.a.mema@gmail.com>
2024-12-11 11:01:30 -0300

    #1 add variables

---
+++ infra/variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+  description = "The AWS Region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Bucket that stores terraform state"
+  type        = string
+}
+
+variable "master_username" {
+  description = "Username for the Master User of Aurora DB"
+  type        = string
+}
+
+variable "master_password" {
+  description = "Password for the Master User of Aurora DB"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  type        = string
+  description = "VPC CIDR value"
+}
+
+variable "public_subnets_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+}
+
+variable "private_subnets_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+}
