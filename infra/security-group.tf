dbe620b64720d864f6841085dfc999228fb91c21
Nicolas Mema <nicolas.a.mema@gmail.com>
2024-12-11 14:47:39 -0300

    #3 add lambda resource

--- infra/security-group.tf
+++ infra/security-group.tf
@@ -1,14 +1,28 @@
+resource "aws_security_group" "lambda" {
+  name        = "LambdaSG"
+  description = "Shared Security Group for accessing Aurora"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_security_group" "aurora" {
   name        = "RDSAuroraSG"
   description = "Allow access to RDS Aurora Postgres"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    description = "PostgreSQL/Aurora"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    # TODO: add Lambda Security Group
+    description     = "PostgreSQL/Aurora"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lambda.id]
   }
 
   egress {
